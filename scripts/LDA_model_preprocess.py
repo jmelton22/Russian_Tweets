a5dcb2593b69af840fe7e1c172e895d9a56c77f5
jmelton22 <jmelton22@gmail.com>
2018-11-18 10:44:39 -0500

    Added LDA model preprocess and testing

---
+++ scripts/LDA_model_preprocess.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', DeprecationWarning)
+    import imp
+warnings.simplefilter('ignore')
+
+import pandas as pd
+import pickle
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+import spacy
+import gensim
+import gensim.corpora as corpora
+
+tweets = pd.read_csv('../tweets/tweets_clean.csv',
+                     header=0,
+                     parse_dates=['date'])
+print('Reading in tweets')
+# Drop tweets reduced to NaN by preprocessing
+tweets = list(tweets.clean_text.dropna())
+
+# Tokenize tweets and remove stop words
+stop_words = set(stopwords.words('english'))
+print('Tokenizing and removing stop words')
+tweets = [[word for word in word_tokenize(tweet)
+           if word not in stop_words]
+          for tweet in tweets]
+
+nlp = spacy.load('en', disable=['parser', 'ner'])
+
+
+def lemmatization(texts, allowed_postags=('NOUN', 'ADJ', 'VERB', 'ADV')):
+    lemmas = []
+    for i, tweet in enumerate(texts):
+        if i % 10000 == 0 or i == len(tweets)-1:
+            print('Lemmatized {} tweets'.format(i))
+        lemmas.append([token.lemma_ for token in nlp(' '.join(tweet)) if token.pos_ in allowed_postags])
+
+    return lemmas
+
+
+tweet_lemmas = lemmatization(tweets)
+
+# Save tweet lemmas to pickle file
+with open('./topic_modeling_objects/lemmas.pkl', 'wb') as f_out:
+    pickle.dump(tweet_lemmas, f_out)
+
+print('Fitting Dictionary')
+id2word = corpora.Dictionary(tweet_lemmas)
+# Save Dictionary to pickle file
+id2word.save('./topic_modeling_objects/dictionary.pkl')
+
+print('Converting docs to bags of words corpus')
+corpus = [id2word.doc2bow(tweet) for tweet in tweet_lemmas]
+print()
+
+# Save corpus to pickle file
+with open('./topic_modeling_objects/corpus.pkl', 'wb') as f_out:
+    pickle.dump(corpus, f_out)
+
+
---
+++ scripts/LDA_model_testing.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', DeprecationWarning)
+    import imp
+warnings.simplefilter('ignore')
+
+from pprint import pprint
+import pickle
+
+import gensim
+import gensim.corpora as corpora
+from gensim.models import CoherenceModel
+
+import matplotlib.pyplot as plt
+
+
+def main():
+    # Load lemmatized tweets
+    with open('./topic_modeling_objects/lemmas.pkl', 'rb') as f:
+        tweet_lemmas = pickle.load(f)
+
+    # Load gensim dictionary
+    id2word = corpora.Dictionary.load('./topic_modeling_objects/dictionary.pkl')
+
+    # Load gensim corpus
+    with open('./topic_modeling_objects/corpus.pkl', 'rb') as f:
+        corpus = pickle.load(f)
+
+    # readable_corpus = [[(id2word[i], freq) for i, freq in cp] for cp in corpus]
+
+    def score_models(dictionary, corpus, texts, limit, start=2, step=3):
+        """
+        Compute c_v coherence scores and log perplexity values for LDA models with various numbers of topics
+
+        :param dictionary: Gensim dictionary
+        :param corpus: Gensim corpus
+        :param texts: List of input texts
+        :param limit: Max num of topics
+        :return:
+            model_list: List of LDA topic models
+            coherences: Coherence values for each LDA model by number of topics
+            log_perplexities: Log perplexity valuess for each LDA model by number of topics
+        """
+        model_list, coherence_list, log_perplexity_list = [], [], []
+        for num_topics in range(start, limit, step):
+            print('Fitting LDA model with {} topics'.format(num_topics))
+            model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                                    id2word=id2word,
+                                                    num_topics=num_topics,
+                                                    random_state=100,
+                                                    update_every=1,
+                                                    chunksize=100,
+                                                    passes=10,
+                                                    alpha='auto',
+                                                    per_word_topics=True)
+            model_list.append(model)
+            log_perplexity_list.append(model.log_perplexity(corpus))
+            print('Fitting Coherence model with {} topics'.format(num_topics))
+            coherence_model = CoherenceModel(model=model,
+                                             texts=texts,
+                                             dictionary=dictionary,
+                                             coherence='c_v')
+            coherence_list.append(coherence_model.get_coherence())
+            print()
+
+        return model_list, coherence_list, log_perplexity_list
+
+    models, coherences, log_perplexities = score_models(dictionary=id2word,
+                                                        corpus=corpus,
+                                                        texts=tweet_lemmas,
+                                                        start=5,
+                                                        limit=50,
+                                                        step=5)
+
+    start, limit, step = 5, 50, 5
+    x = range(start, limit, step)
+
+    fig, ax1 = plt.subplots()
+
+    color = 'tab:blue'
+    ax1.set_xlabel('Num topics')
+    ax1.set_ylabel('Coherence', color=color)
+    ax1.plot(x, coherences, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+
+    color = 'tab:red'
+    ax2.set_ylabel('Log Perplexity', color=color)
+    ax2.plot(x, log_perplexities, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()
+    plt.title('Coherence and Log Perplexity of LDA Models by Number of Topics')
+
+    fig.savefig('../visuals/LDA_model_scores.png')
+
+    plt.show()
+
+    for m, c, p in zip(x, coherences, log_perplexities):
+        print('Num topics {}\nCoherence score = {:.4f}\nLog perplexity = {:.4f}'.format(m, c, p))
+        print()
+
+    # Select optimal model
+    optimal_model = models[coherences.index(max(coherences))]
+    model_topics = optimal_model.show_topics()
+    pprint(model_topics)
+
+
+if __name__ == "__main__":
+    main()
