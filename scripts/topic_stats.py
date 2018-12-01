d13e5411609148f2e6e7e8d53d1147c852927764
jmelton22 <jmelton22@gmail.com>
2018-12-01 11:44:19 -0500

    Added sentiment analysis script and results

--- scripts/topic_stats.py
+++ scripts/topic_stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -25,37 +26,88 @@ topics_daily['sum'] = topics_daily.sum(axis=1)
 topics_daily = topics_daily[topics_daily['sum'] >= 100]
 topics_daily.drop('sum', axis=1, inplace=True)
 
-topics_daily = topics_daily.apply(lambda x: x / x.sum(), axis=1)
+topics_daily_props = topics_daily.apply(lambda x: x / x.sum(), axis=1)
 
 # topics_daily.loc['mean'] = topics_daily.mean()
 # topics_daily.loc['var'] = topics_daily.var()
 
 plt.style.use('seaborn-whitegrid')
 fig = plt.figure(figsize=(12, 6))
-topics_daily.boxplot()
+topics_daily_props.boxplot()
 plt.xlabel('Topic')
 plt.ylabel('Proportion of tweets')
+plt.title('Boxplot of Topics for Daily Tweets')
+
+fig.savefig('../visuals/tweets_prop_boxplot.png')
+
+print(topics_daily_props.idxmax())
+"""
+Topic 0: 2016-10-04: https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_4
+Topic 1: 2016-05-12: https://en.wikipedia.org/wiki/Portal:Current_events/2016_May_12
+Topic 2: 2016-05-08: https://en.wikipedia.org/wiki/Portal:Current_events/2016_May_8
+Topic 3: 2015-12-16: https://en.wikipedia.org/wiki/Portal:Current_events/2015_December_16
+Topic 4: 2016-03-12: https://en.wikipedia.org/wiki/Portal:Current_events/2016_March_12
+Topic 5: 2016-12-14: https://en.wikipedia.org/wiki/Portal:Current_events/2016_December_14
+Topic 6: 2016-02-29: https://en.wikipedia.org/wiki/Portal:Current_events/2016_February_29
+Topic 7: 2016-02-11: https://en.wikipedia.org/wiki/Portal:Current_events/2016_February_11
+Topic 8: 2017-08-03: https://en.wikipedia.org/wiki/Portal:Current_events/2017_August_3
+Topic 9: 2016-07-21: https://en.wikipedia.org/wiki/Portal:Current_events/2016_July_21
+Topic 10: 2016-10-03: https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_3
+Topic 11: 2016-09-22: https://en.wikipedia.org/wiki/Portal:Current_events/2016_September_22
+Topic 12: 2017-06-23: https://en.wikipedia.org/wiki/Portal:Current_events/2017_June_23
+Topic 13: 2015-06-13: https://en.wikipedia.org/wiki/Portal:Current_events/2015_June_13
+Topic 14: 2016-03-22: https://en.wikipedia.org/wiki/Portal:Current_events/2016_March_22
+"""
+
+fig = plt.figure(figsize=(12, 6))
+topics_daily.boxplot()
+plt.xlabel('Topic')
+plt.ylabel('Number of tweets')
 plt.title('Boxplot of Topics for Daily Tweets')
 
 fig.savefig('../visuals/tweets_boxplot.png')
 
 print(topics_daily.idxmax())
 """
-Topic 0: November 02, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_November_2
-Topic 1: February 29, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_February_29
-Topic 2: July 21, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_July_21
-Topic 3: November 23, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_November_23
-Topic 4: September 26, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_September_26
-Topic 5: October 05, 2016:  https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_5
-Topic 6: December 19, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_December_19
-Topic 7: December 16, 2015: https://en.wikipedia.org/wiki/Portal:Current_events/2015_December_16
-Topic 8: October 17, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_17
-Topic 9: November 08, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_November_8
-Topic 10: August 03, 2017: https://en.wikipedia.org/wiki/Portal:Current_events/2017_August_3
-Topic 11: June 23, 2017: https://en.wikipedia.org/wiki/Portal:Current_events/2017_June_23
-Topic 12: September 01, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_September_1
-Topic 13: March 22, 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_March_22
-Topic 14: June 20 2016: https://en.wikipedia.org/wiki/Portal:Current_events/2016_June_20
+Topic 0: 2016-10-06: https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_6
+Topic 1: 2016-11-08: https://en.wikipedia.org/wiki/Portal:Current_events/2016_November_8
+    - US Election
+Topic 2: 2016-12-21: https://en.wikipedia.org/wiki/Portal:Current_events/2016_December_21
+Topic 3: 2016-10-06: https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_6
+Topic 4: 2016-09-18: https://en.wikipedia.org/wiki/Portal:Current_events/2016_September_18
+Topic 5: 2016-12-14: https://en.wikipedia.org/wiki/Portal:Current_events/2016_December_14
+Topic 6: 2016-02-29: https://en.wikipedia.org/wiki/Portal:Current_events/2016_February_29
+Topic 7: 2016-10-06: https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_6
+Topic 8: 2016-10-19: https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_19
+Topic 9: 2016-07-21: https://en.wikipedia.org/wiki/Portal:Current_events/2016_July_21
+Topic 10: 2016-10-06: https://en.wikipedia.org/wiki/Portal:Current_events/2016_October_6
+Topic 11: 2016-12-26: https://en.wikipedia.org/wiki/Portal:Current_events/2016_December_26
+Topic 12: 2017-01-25: https://en.wikipedia.org/wiki/Portal:Current_events/2017_January_25
+Topic 13: 2016-11-14: https://en.wikipedia.org/wiki/Portal:Current_events/2016_November_14
+Topic 14: 2016-03-22: https://en.wikipedia.org/wiki/Portal:Current_events/2016_March_22
 """
+
+tweet_counts = tweets.groupby([tweets.date.dt.date]).sum(axis=0)
+tweet_counts.columns = ['counts']
+tweet_counts = tweet_counts[tweet_counts['counts'] > 0]
+
+tweet_counts['log_counts'] = tweet_counts.counts.apply(lambda x: np.log(x))
+
+
+def logarithm(x):
+    if x > 0:
+        return np.log(x)
+    else:
+        return np.NaN
+
+
+topics_daily = topics_daily.applymap(lambda x: logarithm(x))
+
+fig = topics_daily.plot.kde(figsize=(12, 8))
+plt.xlabel('Log of Number of Tweets')
+plt.ylabel('Proportion of Days')
+plt.title('KDE Plot for Log of Daily Tweets per Topic')
+
+fig.get_figure().savefig('../visuals/topics_kde.png')
 
 plt.show()
